7b6ede055c8b4cd6c0ee2e2c615a7d427faa7b6b
Kinscheck <sighburangel@gmail.com>
2020-09-30 14:02:47 -0400

    hello I am commiting

--- src/com/company/NewRandom.java
+++ src/com/company/NewRandom.java
@@ -6,7 +6,11 @@ public class NewRandom {
         //double r = Math.random();                     //[0.0,1.0]
         //double r = Math.random() *10;                 //[0.0,9.0]
         //int r = (int)(Math.random() * 10);            //[0, 9]
-        int r = (int)(Math.random() * 10 + 1);          //[1,10]
+        //int r = (int)(Math.random() * 10 + 1);          //[1,10]
+        //int r = (int)(Math.random() * 11);
+        //int r = (int)(Math.random() * 2 + 1);
+        //int r = (int)(Math.random() * 12 + 1);\
+        int r = (int)((Math.random() * 6 + 1) + (Math.random() * 6 + 1));
         System.out.println(r);
 
     }
